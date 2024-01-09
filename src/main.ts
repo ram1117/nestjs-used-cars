b25779bdd08c8a6370921177f93208489ff615d5
ram1117 <ramkumark1@outlook.com>
2024-01-09 18:48:15 +0530

    add route for user signup

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
-    whitelist:true          //
+    whitelist:true          //strips any additional properties from request's body that are not in the DTO. for security concern
   }))
   await app.listen(3001);
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,11 +8,14 @@ import {
   Param,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body.email, body.password);
+    this.usersService.create(body.email, body.password);
   }
 }
