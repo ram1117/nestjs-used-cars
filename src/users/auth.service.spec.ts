9d49252adcb0659a460342e75e81b5cfe7c0babb
ram1117 <ramkumark1@outlook.com>
2024-01-21 00:14:21 +0530

    add unit test cases to test user signup

---
+++ src/users/auth.service.spec.ts
@@ -0,0 +1,37 @@
+import { Test } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+
+describe('AuthService', () => {
+  let service: AuthService;
+
+  beforeEach(async () => {
+    const mockUserService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
+
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        { provide: UsersService, useValue: mockUserService },
+      ],
+    }).compile();
+
+    service = module.get(AuthService);
+  });
+
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
+
+  it('create a new user with salted and hashed password ',async()=>{
+    const user  = await service.signup('user1@test.com','fghdsaojnfadsln')
+    expect(user.password).not.toEqual('fghdsaojnfadsln')
+    const [salt,hash] = user.password.split('.')
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  })
+});
