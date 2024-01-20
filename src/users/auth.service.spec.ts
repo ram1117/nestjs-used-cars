ea1c01a680ebea23ae285e8569ae63803222196b
ram1117 <ramkumark1@outlook.com>
2024-01-21 02:59:07 +0530

    add trest case for signup with existing id

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,17 +2,17 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
+import { BadRequestException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
+  const mockUserService: Partial<UsersService> = {
+    find: () => Promise.resolve([]),
+    create: (email: string, password: string) =>
+      Promise.resolve({ id: 1, email, password } as User),
+  };
 
   beforeEach(async () => {
-    const mockUserService: Partial<UsersService> = {
-      find: () => Promise.resolve([]),
-      create: (email: string, password: string) =>
-        Promise.resolve({ id: 1, email, password } as User),
-    };
-
     const module = await Test.createTestingModule({
       providers: [
         AuthService,
@@ -27,11 +27,20 @@ describe('AuthService', () => {
     expect(service).toBeDefined();
   });
 
-  it('create a new user with salted and hashed password ',async()=>{
-    const user  = await service.signup('user1@test.com','fghdsaojnfadsln')
-    expect(user.password).not.toEqual('fghdsaojnfadsln')
-    const [salt,hash] = user.password.split('.')
+  it('create a new user with salted and hashed password ', async () => {
+    const user = await service.signup('user1@test.com', 'fghdsaojnfadsln');
+    expect(user.password).not.toEqual('fghdsaojnfadsln');
+    const [salt, hash] = user.password.split('.');
     expect(salt).toBeDefined();
     expect(hash).toBeDefined();
-  })
+  });
+
+  it('throw error if email id is already in use', async () => {
+    mockUserService.find = () =>
+      Promise.resolve([{ id: 1, email: 'a' } as User]);
+
+    await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
+      BadRequestException,
+    );
+  });
 });
